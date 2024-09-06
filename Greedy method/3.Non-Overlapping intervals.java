9e152f3dbb19c576673817250d428388644db660
Vishnu Thupakula <vishnuthupakulah@gmail.com>
2024-09-06 18:50:01 +0530

    Add files via upload

---
+++ Greedy method/3.Non-Overlapping intervals.java
@@ -0,0 +1,39 @@
+//Given a set of intervals with start and end times, the goal is to find the maximum number of non-overlapping intervals.
+
+import java.util.*;
+class sample {
+    public static void main(String[] args) {
+        Scanner i=new Scanner(System.in);
+        System.out.print("Enter no.of intervals :");
+        int size=i.nextInt();
+        interval[] intervals=new interval[size];
+
+        //Give your input in runtime...
+        for(int j=0;j<size;j++){
+            System.out.print("Enter starting time : ");
+            int a=i.nextInt();
+            System.out.print("Enter ending time : ");
+            int b=i.nextInt();
+            intervals[j]=new interval(a,b);
+        }
+
+        Arrays.sort(intervals,Comparator.comparingInt((interval a)->a.end));
+
+        int total=0,l=0;
+        for(int j=0;j<size;j++){
+            if(intervals[j].start>=l){
+                total++;
+                l=intervals[j].end;
+            }
+        }
+        System.out.println("Maximum no.of non-overlapping intervals : "+total);
+    }
+}
+
+class interval{
+    public int start=0,end=0;
+    interval(int a,int b){
+        start=a;
+        end=b;
+    }
+}
