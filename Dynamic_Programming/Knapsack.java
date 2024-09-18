0e5440cc69d6a2fdbf5170383e197ea0062a909e
Vishnu Thupakula <vishnuthupakulah@gmail.com>
2024-09-18 17:40:09 +0530

    Add files via upload

---
+++ Dynamic_Programming/Knapsack.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner i=new Scanner(System.in);
+        int N=i.nextInt();
+        int[] w=new int[N+1];
+        w[0]=0;
+        int[] p=new int[N+1];
+        p[0]=0;
+        for(int j=1;j<=N;j++){
+          w[j]=i.nextInt();
+          p[j]=i.nextInt();
+        }
+        int cap=i.nextInt();
+        int maxProfit=solve(w,p,cap);
+        System.out.println(maxProfit);
+    }
+    public static int solve(int[] w,int[] p,int cap){
+      int size=w.length;
+      int[][] dp=new int[size][cap+1];
+      for(int i=0;i<size;i++){
+        for(int j=0;j<=cap;j++){
+          if(i==0 || j==0){
+            dp[i][j]=0;
+          }else if(j<w[i]){
+            dp[i][j]=dp[i-1][j];
+          }else{
+            dp[i][j]=Math.max(p[i]+dp[i-1][j-w[i]],dp[i-1][j]);
+          }
+        }
+      }
+      return dp[size-1][cap];
+    }
+}
